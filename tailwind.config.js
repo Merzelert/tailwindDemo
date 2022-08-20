de9bbe7f9cae3a8b4a40d9838ca4df64682156f3
Erik Ivan Retana Lopez <merzelert@gmail.com>
2022-08-20 13:31:12 -0500

    class 11

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {colors:{
+      primary:'#34a853',
+      secondary:'#353535'
+     }},
+  },
+  plugins: [],
+}
